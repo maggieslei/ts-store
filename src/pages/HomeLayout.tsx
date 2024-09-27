b7842a5b59d4af3f32c5725cd757dd6605425c9c
maggieslei <leimaggiese@gmail.com>
2024-09-26 22:59:13 -0400

    feat: add pages, setup router

---
+++ src/pages/HomeLayout.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom';
+
+function HomeLayout() {
+  return (
+    <>
+      <header>header</header>
+      <nav>navbar</nav>
+      <Outlet />
+    </>
+  );
+}
+export default HomeLayout;
