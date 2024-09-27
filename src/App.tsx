7d18079760a0ed29d92eed29eff8fdafae6549d6
maggieslei <leimaggiese@gmail.com>
2024-09-26 21:26:43 -0400

    wip: initial setup

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { Button } from '@/components/ui/button';
+import { useAppSelector } from './hooks';
+
+function App() {
+  const { name } = useAppSelector((state) => state.userState);
+  console.log(name);
+  return (
+    <div>
+      <h1 className="text-7xl font-bold ">App</h1>
+      <Button
+        variant="destructive"
+        size="lg"
+        onClick={() => console.log('it worked!')}
+      >
+        Click Me
+      </Button>
+    </div>
+  );
+}
+export default App;
